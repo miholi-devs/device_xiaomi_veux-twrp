494961758142673a850fe9c9880e869ace010898
itsmeNaraa meledak <maga69505@gmail.com>
2024-09-07 06:13:25 +0000

    import kernel config from moonstone

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,33 +34,25 @@ TARGET_BOOTLOADER_BOARD_NAME := veux
 TARGET_NO_BOOTLOADER := true
 
 # Kernel
-BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_IMAGE_NAME := Image
+VENDOR_CMDLINE := "androidboot.hardware=qcom \
+                   androidboot.memcg=1 \
+		   androidboot.selinux=permissive \
+                   androidboot.usbcontroller=4e00000.dwc3 \
+                   cgroup.memory=nokmem,nosocket \
+                   loop.max_part=7 \
+                   msm_rtb.filter=0x237 \
+                   service_locator.enable=1 \
+                   swiotlb=0 \
+                   pcie_ports=compat \
+                   iptable_raw.raw_before_defrag=1 \
+                   ip6table_raw.raw_before_defrag=1 \
+                   androidboot.init_fatal_reboot_target=recovery"
+
 BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_SEPARATED_DTBO := true
-
-BOARD_BOOT_HEADER_VERSION := 3
-BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOT_HEADER_VERSION)
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-BOARD_RAMDISK_USE_LZ4 := true
-BOARD_USES_GENERIC_KERNEL_IMAGE := true
-
-BOARD_KERNEL_CMDLINE := \
-    androidboot.hardware=qcom \
-    lpm_levels.sleep_disabled=1 \
-    service_locator.enable=1 \
-    androidboot.usbcontroller=4e00000.dwc3 \
-    swiotlb=noforce \
-    loop.max_part=7 \
-    iptable_raw.raw_before_defrag=1 \
-    ip6table_raw.raw_before_defrag=1 \
-    firmware_class.path=/vendor/firmware \
-    ignore_builtin_recovery \
-    audit=0
-
-#TARGET_KERNEL_CONFIG := holi-qgki_defconfig
-#TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6375
-#TARGET_KERNEL_NO_GCC := true
+BOARD_KERNEL_BASE := 0x00000000
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CLANG_COMPILE := true
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_BOOT_HEADER_VERSION := 3
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/veux/kernel
